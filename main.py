235cad2b806b904bf4c5c9e9b17ed1291ace1add
Taky0nDon <126304313+Taky0nDon@users.noreply.github.com>
2023-05-04 18:59:13 -0400

    remove cards from deck once they are shown and edit CSV to only include unknown cards

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ LANGUAGE_NAME_FONT = ("Ariel", 40, "italic")
 BACK = "./images/card_back.png"
 FRONT = "./images/card_front.png"
 WORD_FONT = ("Ariel", 60, "bold")
+WORD_POSITION = (400, 263)
 
 CHECK = "./images/right.png"
 CROSS = "./images/wrong.png"
@@ -14,38 +15,64 @@ CROSS = "./images/wrong.png"
 #DATAFRAME
 data = pd.read_csv("./data/french_words.csv")
 
+# DATA DICTIONARY
+current_deck = data.to_dict(orient="records")
+# a list of dictionaries where each dict
+# has two keys, "French" and "English"
+
 # FRONT OF FLASH CARD
-word_number = 0
-known_words_index = []
+current_card = None
+shown_words = []
 
 window = tk.Tk()
 
 # todo figure out how to add a word to list when check is cicked
-def mark_known():
-    pass
 
+def word_is_known():
+    global current_card, current_deck
+    if len(current_deck) == 0 and len(shown_words) == 0:
+        new_canvas = tk.Canvas()
+        new_canvas.create_text(600, 263, text="You know all the words!")
+    else:
+        current_deck.remove(current_card)
+        show_front()
+    # save a new CSV of the words that are in word_pairs and not in known_words
+
+def save_new_csv():
+    unknown_words = pd.DataFrame(shown_words)
+    unknown_words.to_csv("./data/french_words.CSV")
 
 def show_front():
-    index = rnd.randint(0,101)
-    while index in known_words_index:
-        index = rnd.randint(0, 101)
-    canvas.create_image(400, 263, image=card_front)
-    canvas.create_text((400, 150), text=data.columns[0], font=LANGUAGE_NAME_FONT)
-    canvas.create_text((400, 263), text=data.French[index], font=WORD_FONT)
-    window.after(3000, lambda: show_back(index))
-    return index
+    global current_card, timer, current_deck, i
+    current_deck = [card for card in current_deck if card not in shown_words]
+    window.after_cancel(timer)
+    if len(current_deck) < 1:
+        canvas.itemconfig(language, text="Done", fill="black")
+        canvas.itemconfig(word, text="All words tested", fill="black")
+        check_button.config(command= lambda: None)
+        cross_button.config(command= lambda: None)
+    else:
+        current_card = rnd.choice(current_deck)
+        canvas.itemconfig(card_side, image=card_back)
+        canvas.itemconfig(language, text="French", fill="black")
+        canvas.itemconfig(word, text=current_card['French'], fill="black")
+        timer = window.after(3000, show_back)
+#
+def show_back():
+    global current_card
+    canvas.itemconfig(card_side, image=card_back)
+    canvas.itemconfig(language, text="English", fill="white")
+    canvas.itemconfig(word, text=current_card['English'], fill="white")
 
-
-def show_back(index):
-    canvas.create_image(400, 263, image=card_back)
-    canvas.create_text((400, 150), text=data.columns[1], font=LANGUAGE_NAME_FONT)
-    canvas.create_text((400, 263), text=data.English[index], font=WORD_FONT)
-    window.after(3000, show_front)
-
-
+def dont_know():
+    global current_card
+    current_deck.remove(current_card)
+    shown_words.append(current_card)
+    show_front()
 # WINDOW
 window.config(bg=BACKGROUND_COLOR, padx=50, pady=50)
 window.title("Flash Card App")
+timer = window.after(3000, show_back)
 
 # IMAGES
 card_front = tk.PhotoImage(file=FRONT)
@@ -55,18 +82,21 @@ cross = tk.PhotoImage(file=CROSS)
 
 # CANVAS
 canvas=tk.Canvas(width=800, height=526, borderwidth=0, highlightthickness=0, bg=BACKGROUND_COLOR)
-index = show_front()
-print(show_front())
+card_side = canvas.create_image(400, 263, image=card_front)
+language = canvas.create_text((400, 150), font=LANGUAGE_NAME_FONT)
+word = canvas.create_text(WORD_POSITION, font=WORD_FONT)
 
 
 # BUTTONS
-check_button = tk.Button(image=check, borderwidth=0, highlightthickness=0)
-cross_button = tk.Button(image=cross, borderwidth=0, highlightthickness=0)
+check_button = tk.Button(image=check, borderwidth=0, highlightthickness=0, command=word_is_known)
+cross_button = tk.Button(image=cross, borderwidth=0, highlightthickness=0, command=dont_know)
 
 # GRID
 canvas.grid(column=0, row=0, columnspan=2)
 cross_button.grid(column=0, row=1, pady=50)
 check_button.grid(column=1, row=1)
 
+show_front()
 
 window.mainloop()
+save_new_csv()
